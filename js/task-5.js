e78c07064e13fdff4c0c20b4fd84fa17c7f81287
Tymofii <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-04-29 17:54:05 +0300

    Initial commit

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+/* Завдання 5: Зміна кольору фону */
+const colorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+colorBtn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+
